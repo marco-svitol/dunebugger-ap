2c177eb56018d85a1772f9eeacb3c73b07d39518
marco <marco.cambon@gmail.com>
2023-12-27 00:41:21 +0100

    ddd

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exec = require('child_process').exec;
+const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
@@ -9,18 +9,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve HTML configuration page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/config.html');
+  res.sendFile(__dirname + '/config.html');
 });
 
 // Handle form submission
 app.post('/configure', (req, res) => {
-    const { ssid, password } = req.body;
+  const { ssid, password } = req.body;
 
-    // TODO: Switch to client mode and connect to specified WiFi
+  // Set WiFi configuration using Network Manager
+  setWifiConfiguration(ssid, password);
 
-    res.send('Configuration successful. Please reboot your Raspberry Pi.');
+  res.send('Configuration successful. Please reboot your device.');
 });
 
+function setWifiConfiguration(ssid, password) {
+  const command = `nmcli device wifi connect "${ssid}" password "${password}"`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error setting WiFi configuration: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`Error setting WiFi configuration: ${stderr}`);
+      return;
+    }
+
+    console.log(`WiFi configuration set successfully: ${stdout}`);
+
+    // Create the flag file indicating WiFi configuration is completed
+    createFlagFile();
+  });
+}
+
+// Start the Express server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
+
+function createFlagFile() {
+    const flagFilePath = '/etc/wifi_configured_flag';
+  
+    fs.writeFile(flagFilePath, '', (err) => {
+      if (err) {
+        console.error(`Error creating flag file: ${err.message}`);
+      } else {
+        console.log(`Flag file created: ${flagFilePath}`);
+      }
+    });
+  }
