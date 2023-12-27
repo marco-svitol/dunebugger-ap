0a44148636a90414d8e6d7ed6240f74b97192605
marco <marco.cambon@gmail.com>
2023-12-27 01:56:13 +0100

    dd

--- app/app.js
+++ app/app.js
@@ -76,8 +76,8 @@ function setWifiConfiguration(ssid, password, callback) {
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error setting WiFi configuration: ${stderr}`);//${error.message}`);
-      callback(error.message);
+      console.error(`Error setting WiFi configuration: ${error.message}`);
+      callback(stderr);
       return;
     }
 
